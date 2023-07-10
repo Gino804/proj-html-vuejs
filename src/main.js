17932599aa06db34d8c09f9d65099063e5dfc58d
Gino804 <giacomo.p03@gmail.com>
2023-07-10 16:19:16 +0200

    Added fontawesome

--- src/main.js
+++ src/main.js
@@ -1,4 +1,32 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+import { faFacebookF } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faGooglePlusG } from "@fortawesome/free-brands-svg-icons";
+import { faLock } from "@fortawesome/free-solid-svg-icons";
+import { faPinterest } from "@fortawesome/free-brands-svg-icons";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faShoppingBasket } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faFacebookF);
+library.add(faTwitter);
+library.add(faLinkedinIn);
+library.add(faInstagram);
+library.add(faGooglePlusG);
+library.add(faLock);
+library.add(faPinterest);
+library.add(faStar);
+library.add(faShoppingBasket);
+library.add(faArrowRight);
+library.add(faArrowLeft);
+
+createApp(App).component("FontAwesomeIcon", FontAwesomeIcon).mount("#app");
