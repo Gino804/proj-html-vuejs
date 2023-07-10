41dc57f2dc15724cfe3b3788a76a46911ec0df5b
Gino804 <giacomo.p03@gmail.com>
2023-07-10 18:32:11 +0200

    Added header searchbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,11 @@
 <script>
 import HeaderSocials from './HeaderSocials.vue';
+import BaseSearch from './BaseSearch.vue';
 
 export default {
     components: {
-        HeaderSocials
+        HeaderSocials,
+        BaseSearch
     }
 }
 </script>
@@ -12,18 +14,31 @@ export default {
     <header>
         <HeaderSocials />
         <div class="separator"></div>
+        <div class="container">
+            <img src="../assets/img/cropped-logo.png">
+            <BaseSearch :placeholder="'Search ...'" :buttonText="'Search'" />
+        </div>
+        <div class="separator"></div>
     </header>
 </template>
 
 <style lang="scss" scoped>
+@use "../assets/scss/partials/vars" as *;
+
 header {
     color: white;
     margin-top: 25px;
 
     .separator {
         height: 1px;
-        background-color: #131B24;
+        background-color: $lightgrey;
         margin: 25px 0;
+    }
+
+    .container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
     }
 }
 </style>
---
+++ src/components/BaseSearch.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    props: {
+        placeholder: String,
+        buttonText: String
+    }
+}
+</script>
+
+<template>
+    <div>
+        <input type="text" :placeholder="placeholder">
+        <button>{{ buttonText }}</button>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../assets/scss/partials/vars" as *;
+
+input {
+    background-color: $darkblue;
+    border: 1px solid $lightgrey;
+    border-left: none;
+    border-right: none;
+}
+
+button {
+    background-color: $green;
+    border: none;
+}
+
+input,
+button {
+    padding: 15px;
+    color: white;
+}
+</style>
